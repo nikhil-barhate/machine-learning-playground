97b69f47f229e79ba24c400e37e0c09dbb2d9d7a
Nikhil Barhate <nikhilbarhate99@gmail.com>
2018-06-07 14:39:31 +0530

    Update mnist_classifier.py

--- mnist_classifier.py
+++ mnist_classifier.py
@@ -3,9 +3,9 @@ from tensorflow.examples.tutorials.mnist import input_data
 
 mnist = input_data.read_data_sets("/tmp/data/", one_hot=True)
 
-n_nodes_hl1 = 500
-n_nodes_hl2 = 500
-n_nodes_hl3 = 500
+n_nodes_hl1 = 300
+n_nodes_hl2 = 400
+n_nodes_hl3 = 100
 
 n_classes = 10
 batch_size = 100
